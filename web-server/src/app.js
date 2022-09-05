2a66059bbca6a167d76a5f766bcff0e5d1282365
Duc Le <duc.le@opswat.com>
2022-09-05 15:19:54 +0700

    update code

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -9,6 +9,7 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials')
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
@@ -70,6 +71,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('App listen on port 3000.')
+app.listen(port, () => {
+    console.log('App listen on port', port)
 })
